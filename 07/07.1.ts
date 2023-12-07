eaf97683b5cbc0ee74598ac36e961a7c05aead91
Yo-Dan <yodan.and.the.sixth.character@gmail.com>
2023-12-07 01:34:15 -0500

    Day7: solution

---
+++ 07/07.1.ts
@@ -0,0 +1,87 @@
+import {readFileByLine} from "../util";
+
+let sum = 0;
+
+interface HandData {
+  cards: number[];
+  bid: number;
+  hand_strength: number;
+}
+
+const hands: HandData[] = [];
+
+const conversion = new Map([
+  ['2', 2],
+  ['3', 3],
+  ['4', 4],
+  ['5', 5],
+  ['6', 6],
+  ['7', 7],
+  ['8', 8],
+  ['9', 9],
+  ['T', 10],
+  ['J', 11],
+  ['Q', 12],
+  ['K', 13],
+  ['A', 14],
+]);
+
+function cardsToHandStrength(cards: number[]): number {
+  const m = new Map<number, number>();
+  for (const c of cards) {
+    if (m.has(c)) {
+      m.set(c, m.get(c) + 1);
+    } else {
+      m.set(c, 1);
+    }
+  }
+  switch(m.size) {
+    case 1:
+      return 7; // 5 of a kind
+    case 2:
+      if ([...m.values()].some(v => v === 4)) {
+        return 6; // 4 of a kind
+      }
+      return 5; // full house
+    case 3:
+      if ([...m.values()].some(v => v === 3)) {
+        return 4; // 3 of a kind
+      }
+      return 3; // 2 pair
+    case 4:
+      return 2; // 1 pair
+    case 5:
+      return 1; // nuthin
+    default:
+      console.error('err');
+      return 0;
+  }
+}
+
+readFileByLine('input', async (line: string) => {
+  if (!line) {
+    return;
+  }
+  const cards = line.split(' ')[0].trim().split('').map(c => conversion.get(c));
+  hands.push({
+    cards: cards,
+    bid: parseInt(line.split(' ')[1].trim()),
+    hand_strength: cardsToHandStrength(cards),
+  });
+}).then(() => {
+  hands.sort((a, b) => {
+    if (a.hand_strength !== b.hand_strength) {
+      return a.hand_strength - b.hand_strength;
+    }
+    for (const [i, c] of a.cards.entries()) {
+      if (c !== b.cards[i]) {
+        return c - b.cards[i];
+      }
+    }
+    return 0;
+  });
+  for (const [i, h] of hands.entries()) {
+    sum += (i + 1) * h.bid;
+  }
+  console.log(sum);
+});
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Advent of Code 2023
 
-Day6: solution
+Day7: solution
